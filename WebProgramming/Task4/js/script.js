36a8178709320cdfa82bcc2dac5c7e1616ec4ffa
AndriyVohar <126296269+AndriyVohar@users.noreply.github.com>
2023-03-23 11:55:13 +0200

    Add files via upload

--- WebProgramming/Task4/js/script.js
+++ WebProgramming/Task4/js/script.js
@@ -1,8 +1,8 @@
 //Task 1. З клавіатури вводяться числа до тих пір, поки послідовність є монотонною (зростаючою або спадною).
-let x0=Number(prompt("Enter number"));
-let x1=Number(prompt("Enter number"));
-let div = x1-x0;
-let result = 2;
+var x0=Number(prompt("Enter number"));
+var x1=Number(prompt("Enter number"));
+var div = x1-x0;
+var result = 2;
 do{
     x0 = Number(prompt("Enter number"));
     if(x0-x1 == div){
@@ -14,42 +14,40 @@ do{
 }while(true)
 alert("Result = "+result);
 
-/*
+
 //Task 2. Знайти суму цифр числа, що знаходяться між першою цифрою та останньою цифрою.
-let num = Number(prompt("Enter number"));
-let result = 0;
+var num = Number(prompt("Enter number"));
+var result = 0;
 num = Math.floor(num/10);
 while(`${num}`.length>1){
-    console.log(num%10);
     result+=num%10;
     num = Math.floor(num/10);
 }
 alert(result);
 
 //Task 4. Необхідно підсумовувати всі непарні цілі числа в діапазоні, який користувач введе з клавіатури.
-let start = Number(prompt("Введіть початок діапазону")); 
-let stop = Number(prompt("Введіть кінець діапазону"));
-let result = 0;
+var start = Number(prompt("Введіть початок діапазону")); 
+var stop = Number(prompt("Введіть кінець діапазону"));
+var result = 0;
 if(start%2==0) start+=1;
 for(start; start<stop; start+=2){
     result+=start;
 }
 alert(result);
 
-//Task 5. Написати програму, яка показуватиме на екран квадрат числа, введеного користувачем. Користувач повинен сам вирішувати – вийти із програми або продовжити введення.
+//Task 6. Написати програму, яка показуватиме на екран квадрат числа, введеного користувачем. Користувач повинен сам вирішувати – вийти із програми або продовжити введення.
 do{
-    let num = Number(prompt("Enter number"));
+    var num = Number(prompt("Enter number"));
     alert(num**2);
-    let n = Number(prompt("Введіть, що ви хочете робити далі: \n 1 - залишитись в програмі \n 2 - вийти з програми"))
+    var n = Number(prompt("Введіть, що ви хочете робити далі: \n 1 - залишитись в програмі \n 2 - вийти з програми"))
     if(n == 2) break;
 }while(true)
 
 //Task 30. З клавіатури вводяться числа і підсумовуються. Знайти кількість введених чисел, коли їхня сума перевищила 100.
-let result = 0;
-let i = 0;
+var result = 0;
+var i = 0;
 while(i<100){
     i += Number(prompt("Enter number"));
     result++;
 }
 alert("Result = "+result);
-*/
